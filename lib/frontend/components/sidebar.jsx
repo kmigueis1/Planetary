fbc4d3618ca3d02884b57bcea132f924b7a60ee0
Kevin Migueis <kmigueis1@gmail.com>
2017-11-10 22:36:21 -0500

    Style side bar

--- lib/frontend/components/sidebar.jsx
+++ lib/frontend/components/sidebar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Planet from '../../planet';
 import * as Util from '../../util';
-import InputRange from 'react-input-range';
+
 
 
 class SideBar extends React.Component {
@@ -11,10 +11,22 @@ class SideBar extends React.Component {
     super(props);
 
     this.state = {
-      radius: "5"
+      name: "earth",
+      radius: "5",
+      speed: "0",
+      orbitalRadius: "50",
+      orbitalCenter: [0,0,0],
+      color: 0xffff00
     };
     this.handleSubmit = this.handleSubmit.bind(this);
     this.setRadius = this.setRadius.bind(this);
+    this.setName = this.setName.bind(this);
+    this.setSpeed = this.setSpeed.bind(this);
+    this.setOrbitalRadius = this.setOrbitalRadius.bind(this);
+    this.setOrbitalCenterX = this.setOrbitalCenterX.bind(this);
+    this.setOrbitalCenterY = this.setOrbitalCenterY.bind(this);
+    this.setOrbitalCenterZ = this.setOrbitalCenterZ.bind(this);
+
   }
 
   componentDidMount(){
@@ -23,23 +35,81 @@ class SideBar extends React.Component {
 
 
   handleSubmit(e){
+    let name = this.state.name;
     let radius = parseInt(this.state.radius);
-    this.props.addPlanet(new Planet(radius, 30, 200, [0,0,0], Util.planetColors("red"), "superearth"))
+    let speed = parseInt(this.state.speed);
+    let orbitalRadius = parseInt(this.state.orbitalRadius);
+    let orbitalCenter = this.state.orbitalCenter;
+    let color = this.state.color;
+
+    this.props.addPlanet(new Planet(radius, speed, orbitalRadius, orbitalCenter,  color, name))
+  }
+
+  setName(e){
+    this.setState({name: e.target.value})
   }
 
   setRadius(e){
     this.setState({radius: e.target.value})
   }
-  // <InputRange minValue={1} maxValue={100} value={this.state.radius} onChange={this.setRadius}/>
-// <input type="range" min="1" max="100" defaultValue="5" onInput={this.setRadius}/>
+
+  setSpeed(e){
+    this.setState({speed: e.target.value})
+  }
+
+  setOrbitalRadius(e){
+    this.setState({orbitalRadius: e.target.value})
+  }
+
+  setOrbitalCenterX(e){
+    this.setState({orbitalCenter: [e.target.value, this.state.orbitalCenter[1], this.state.orbitalCenter[2]]})
+  }
+
+  setOrbitalCenterY(e){
+    this.setState({orbitalCenter: [this.state.orbitalCenter[0], e.target.value, this.state.orbitalCenter[2]]})
+  }
+
+  setOrbitalCenterZ(e){
+    this.setState({orbitalCenter: [this.state.orbitalCenter[0], this.state.orbitalCenter[1], e.target.value]})
+  }
 
   render(){
     return (
       <div className="side-bar-container">
 
-            <input type="text"  onChange={this.setRadius} value={this.state.radius}/>
+        <div className="add-planet-form">
+          <span>Planet Name</span>
+            <div>
+              <input type="text" value={this.state.name} onChange={this.setName} />
+            </div>
 
-        <button onClick={this.handleSubmit}>AddPlanet</button>
+          <span>Planet Radius</span>
+            <div>
+              <input type="range" min="1" max="100" value={this.state.radius} onChange={this.setRadius} />
+              <span>{this.state.radius}</span>
+            </div>
+
+          <span>Speed</span>
+            <div>
+              <input type="range" min="0" max="100" value={this.state.speed} onChange={this.setSpeed} />
+              <span>{this.state.speed}</span>
+            </div>
+
+          <span>Orbital Radius</span>
+            <div>
+              <input type="range" min="1" max="100" value={this.state.orbitalRadius} onChange={this.setOrbitalRadius} />
+              <span>{this.state.orbitalRadius}</span>
+            </div>
+
+          <span>Orbital Center</span>
+            <div>
+              <input className="orb-center-input" type="number" value={this.state.orbitalCenter[0]} onChange={this.setOrbitalCenterX} />
+              <input className="orb-center-input" type="number" value={this.state.orbitalCenter[1]} onChange={this.setOrbitalCenterY} />
+              <input className="orb-center-input" type="number" value={this.state.orbitalCenter[2]} onChange={this.setOrbitalCenterZ} />
+            </div>
+
+          <button onClick={this.handleSubmit}>AddPlanet</button>
+        </div>
       </div>
     );
   }

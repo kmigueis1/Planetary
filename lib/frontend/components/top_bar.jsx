b10b11232026eb90ed8975741e166b5d30e05ca0
Kevin Migueis <kmigueis1@gmail.com>
2017-11-28 13:47:06 -0500

    Add favicon and title icon

--- lib/frontend/components/top_bar.jsx
+++ lib/frontend/components/top_bar.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const TopBar = () => {
   return (
     <div className="top-bar">
-      <div className="title">Planetary</div>
+      <div className="title"><span className="title-span">Planetary</span></div><div><img src="../../../images/Planetary_Icon.png"></img></div>
     </div>
   );
 }

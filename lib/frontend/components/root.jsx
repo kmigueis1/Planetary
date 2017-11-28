1c4ff62d5efdf9726352fce7ce4e4d148aab419b
Kevin Migueis <kmigueis1@gmail.com>
2017-11-28 12:37:35 -0500

    Add header and footer and style links

--- lib/frontend/components/root.jsx
+++ lib/frontend/components/root.jsx
@@ -3,12 +3,16 @@ import { Provider } from 'react-redux';
 import React from 'react';
 import SideBarContainer from './sidebar_container';
 import CanvasContainer from './canvas_container';
+import TopBar from './top_bar';
+import Footer from './footer';
 
 const Root = ({store}) => (
   <Provider store={store}>
     <div className="app-container">
       <CanvasContainer />
-        <SideBarContainer />
+      <SideBarContainer />
+      <TopBar />
+      <Footer />
     </div>
   </Provider>
 
---
+++ lib/frontend/components/top_bar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TopBar = () => {
+  return (
+    <div className="top-bar">
+      <div className="title">Planetary</div>
+    </div>
+  );
+}
+
+export default TopBar;
